7533f8d61f9fba23969cd4e292b899b0b9e0a301
Itai Bar-Natan <itaibn3141@gmail.com>
2022-03-02 18:55:09 -0800

    first commit

---
+++ BB.scm
@@ -0,0 +1,199 @@
+9
+99
+999
+9999
+#e9e9
+#e9e99
+#e9e999
+#e9e9999
+#e9e99999
+#e9e999999
+#e9e9999999
+#e9e99999999
+#e9e999999999
+#e9e9999999999
+(expt 9 #e9e99)
+(expt 9 #e9e999)
+(expt 9 #e9e9999)
+(expt 9 #e9e99999)
+(expt 9 #e9e999999)
+(expt 9 #e9e9999999)
+(expt 9 #e9e99999999)
+(expt 9(expt 9 #e9e9))
+(expt 9(expt 9 #e9e99))
+(expt 9(expt 9 #e9e999))
+(expt 9(expt 9 #e9e9999))
+(define ,a(expt a a)),,,,9
+(define ,a(expt a a)),,,,,9
+(define ,a(expt a a)),,,,,,9
+(define ,a(expt a a)),,,,,,,9
+(define ,a(expt a a)),,,,,,,,9
+(define ,a(expt a a)),,,,,,,,,9
+(define ,a(expt a a)),,,,,,,,,,9
+(define ,a(expt a a)),,,,,,,,,,,9
+(define ,a(expt a a)),,,,,,,,,,,,9
+(define ,a(expt a a)),,,,,,,,,,,,,9
+(define ,a(expt a a)),,,,,,,,,,,,,,9
+(define ,a(expt a a)),,,,,,,,,,,,,,,9
+(define ,a(expt a a)),,,,,,,,,,,,,,,,9
+(define ,a(expt a(expt a a))),,,,,,,,,9
+(define ,a(expt a(expt a a))),,,,,,,,,,9
+(define ,a(expt a(expt a a))),,,,,,,,,,,9
+(define ,a(expt 9(if(> a 0),(- a 1)9))),,9
+(define ,a(expt 9(if(> a 0),(- a 1)9))),,,9
+(define ,a(expt 9(if(> a 0),(- a 1)9))),,,,9
+(define ,a(expt 9(if(> a 0),(- a 1)9))),,,,,9
+(define ,a(expt 9(if(> a 0),(- a 1)9))),,,,,,9
+(define ,a(expt 9(if(> a 0),(- a 1)9))),,,,,,,9
+(define ,a(expt 9(if(> a 0),(- a 1)9))),,,,,,,,9
+(define ,a(expt 9(if(> a 0),(- a 1)9))),,,,,,,,,9
+(define ,a(expt 9(if(> a 0),(- a 1)9))),,,,,,,,,,9
+(define ,a(expt 9(if(> a 0),(- a 1)9))),,,,,,,,,,,9
+(define ,a(expt 9(if(> a 0),(- a 1)9))),,,,,,,,,,,,9
+(define ,a(expt 9(if(> a 0),(- a 1)9))),,,,,,,,,,,,,9
+(define ,a(expt 9(if(> a 0),(- a 1)9))),,,,,,,,,,,,,,9
+(define ,a(+(if(>(remainder a 8)0)(+ ,,(- a 8)6)a)9)),9
+(define ,a(+(if(>(remainder a 9)0)(+ ,,(- a 9)2)a)8)),98
+(define ,a(+(if(>(remainder a 9)0)(+ ,,(- a 9)2)a)8)),998
+(define ,a(+(if(>(remainder a 13)0)(+ ,,(- a 13)8)a)9)),87
+(define ,a(+(if(>(remainder a 98)0)(+ ,,(- a 98)2)a)97)),99
+(define ,a(*(if(<(remainder a 575)2)a(* ,,(- a 575)9))-8)),7
+;(define ,a(*(if(<(remainder a 647)2)a(* ,,(- a 647)8))-9)),3
+(define ,a(*(if(<(remainder a 647)2)a(* ,,(- a 647)8))-9)),74
+;(define ,a(*(if(<(remainder a 647)2)a(* ,,(- a 647)8))-9)),81
+(let a((b 9)(c 999))(+(if(>(* b c)0)(a(a(- b 1)c)(- c 1))c)b))
+(let a((b 9)(c 9999))(+(if(>(* b c)0)(a(a(- b 1)c)(- c 1))c)b))
+(let a((b 9)(c #e9e9))(+(if(>(* b c)0)(a(a(- b 1)c)(- c 1))c)b))
+(let a((b 9)(c #e9e99))(+(if(>(* b c)0)(a(a(- b 1)c)(- c 1))c)b))
+(let a((b 9)(c #e9e999))(+(if(>(* b c)0)(a(a(- b 1)c)(- c 1))c)b))
+(let a((b 9)(c #e9e9999))(+(if(>(* b c)0)(a(a(- b 1)c)(- c 1))c)b))
+(define(a b c)(+(if(>(* b c)0)(a(- b 1)(a b(- c 1)))9)c))(a(a 9 9)9)
+(define(a b c)(+(if(>(* b c)0)(a(- b 1)(a b(- c 1)))b)c))(a(a 99 9)9)
+(define(a b c)(+(if(>(* b c)0)(a(- b 1)(a b(- c 1)))b)c))(a(a 999 9)9)
+(define(a b c)(+(if(>(* b c)0)(a(- b 1)(a b(- c 1)))b)c))(a(a 9999 9)9)
+(define(a b c)(+(if(>(* b c)0)(a(- b 1)(a b(- c 1)))9)c))(a(a(a 9 9)9)9)
+(define(a b c)(+(if(>(* b c)0)(a(- b 1)(a b(- c 1)))b)c))(a(a(a 99 9)9)9)
+(define(a b c)(+(if(>(* b c)0)(a(- b 1)(a b(- c 1)))b)c))(a(a(a 999 9)9)9)
+(define(a b c)(+(if(>(* b c)0)(a(- b 1)(a b(- c 1)))b)c))(a(a(a 9999 9)9)9)
+(define ,x(let a((b x)(c x))(+(if(>(* b c)0)(a(a(- b 1)c)(- c 1))9)b))),,,,9
+(define ,x(let a((b x)(c x))(+(if(>(* b c)0)(a(a(- b 1)c)(- c 1))9)b))),,,,,9
+(define ,x(let a((b x)(c x))(+(if(>(* b c)0)(a(a(- b 1)c)(- c 1))9)b))),,,,,,9
+(let a((b 9)(c 8)(d 7))(*(if(> b c d 0)(a(a(a(- b 1)c d)(- c 1)d)b(- d 1))b)9))
+(let a((b 9)(c 9)(d 9))(+(if(>= b c d 0)(a(a(a(- b 1)c d)b(- d 1))(- c 1)d)9)b))
+(let a((b 9)(c 9)(d 9))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)9(- d 1))d)b)c))
+(let a((b 9)(c 9)(d 99))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)d(- d 1))d)b)c))
+(let a((b 9)(c 9)(d 999))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)d(- d 1))d)b)c))
+(let a((b 9)(c 9)(d 9999))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)d(- d 1))d)b)c))
+(let a((b 9)(c 9)(d #e9e9))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)d(- d 1))d)b)c))
+(let a((b 9)(c 9)(d #e9e99))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)d(- d 1))d)b)c))
+(let a((b 9)(c 9)(d #e9e999))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)d(- d 1))d)b)c))
+(let a((b 9)(c 9)(d #e9e9999))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)d(- d 1))d)b)c))
+(define(a b c d)(+(if(>(* b c d)0)(a(a(a(- b 1)c d)(- c 1)b)c(- d 1))c)b))(a 9(a 9 9 9)9)
+(define(a b c d)(+(if(>(* b c d)0)(a(a(a(- b 1)c d)(- c 1)b)c(- d 1))c)b))(a 9(a 9 99 9)9)
+(define(a b c d)(+(if(>(* b c d)0)(a(a(a(- b 1)c d)(- c 1)b)c(- d 1))c)b))(a 9(a 9 999 9)9)
+(define(a b c d)(+(if(>(* b c d)0)(a(a(a(- b 1)c d)(- c 1)b)c(- d 1))c)b))(a 9(a 9 9999 9)9)
+(define(a b c d)(+(if(>(* b c d)0)(a(a(a(- b 1)c d)(- c 1)b)c(- d 1))c)b))(a 9(a 9 #e9e9 9)9)
+(define(a b c d)(+(if(>(* b c d)0)(a(a(a(- b 1)c d)(- c 1)b)c(- d 1))c)b))(a 9(a 9 #e9e99 9)9)
+(define(a b c d)(+(if(>(* b c d)0)(a(a(a(- b 1)c d)(- c 1)b)c(- d 1))c)b))(a 9(a 9(a 9 9 9)9)9)
+(define(a b c d)(+(if(>(* b c d)0)(a(a(a(- b 1)c d)(- c 1)b)c(- d 1))c)b))(a 9(a 9(a 9 99 9)9)9)
+(define ,x(let a((b x)(c x)(d x))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)9(- d 1))d)b)c))),,,,9
+(define ,x(let a((b x)(c x)(d x))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)9(- d 1))d)b)c))),,,,,9
+(define ,x(let a((b x)(c x)(d x))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)9(- d 1))d)b)c))),,,,,,9
+(define ,x(let a((b x)(c x)(d x))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)9(- d 1))d)b)c))),,,,,,,9
+(define ,x(let a((b x)(c x)(d x))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)9(- d 1))d)b)c))),,,,,,,,9
+(define ,x(let a((b x)(c x)(d x))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)9(- d 1))d)b)c))),,,,,,,,,9
+(define ,x(let a((b x)(c x)(d x))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)9(- d 1))d)b)c))),,,,,,,,,,9
+(define ,x(let a((b x)(c x)(d x))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)9(- d 1))d)b)c))),,,,,,,,,,,9
+(define ,x(let a((b x)(c x)(d x))(+(if(>(* b c d)0)(a(- b 1)(a(a b(- c 1)d)9(- d 1))d)b)c))),,,,,,,,,,,,9
+(let a((b 9)(c 9)(d 9)(e 9))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)9))c d(- e 1))e)b))
+(let a((b 9)(c 9)(d 99)(e 9))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)d))c d(- e 1))e)b))
+(let a((b 9)(c 9)(d 999)(e 9))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)d))c d(- e 1))e)b))
+(let a((b 9)(c 9)(d 9999)(e 9))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)d))c d(- e 1))e)b))
+(let a((b 9)(c 9)(d #e9e9)(e 9))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)d))c d(- e 1))e)b))
+(let a((b 9)(c 9)(d #e9e99)(e 9))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)d))c d(- e 1))e)b))
+(let a((b 9)(c 9)(d #e9e999)(e 9))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)d))c d(- e 1))e)b))
+(let a((b 9)(c 9)(d #e9e9999)(e 9))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)d))c d(- e 1))e)b))
+(let a((b 9)(c 9)(d #e9e99999)(e 9))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)d))c d(- e 1))e)b))
+(define(a b c d e)(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)9))c d(- e 1))e)b))(a 9 9(a 9 9 9 9)9)
+(define(a b c d e)(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)d))c d(- e 1))e)b))(a 9 9(a 9 9 99 9)9)
+(define(a b c d e)(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)d))c d(- e 1))e)b))(a 9 9(a 9 9 999 9)9)
+(define(a b c d e)(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)d))c d(- e 1))e)b))(a 9 9(a 9 9 9999 9)9)
+(define(a b c d e)(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)d))c d(- e 1))e)b))(a 9 9(a 9 9 #e9e9 9)9)
+(define(a b c d e)(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)d))c d(- e 1))e)b))(a 9 9(a 9 9 #e9e99 9)9)
+(define ,x(let a((b x)(c x)(d x)(e x))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)9))c d(- e 1))e)b))),,,9
+(define ,x(let a((b x)(c x)(d x)(e x))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)9))c d(- e 1))e)b))),,,,9
+(define ,x(let a((b x)(c x)(d x)(e x))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)9))c d(- e 1))e)b))),,,,,9
+(define ,x(let a((b x)(c x)(d x)(e x))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)9))c d(- e 1))e)b))),,,,,,9
+(define ,x(let a((b x)(c x)(d x)(e x))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)9))c d(- e 1))e)b))),,,,,,,9
+(define ,x(let a((b x)(c x)(d x)(e x))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)9))c d(- e 1))e)b))),,,,,,,,9
+(define ,x(let a((b x)(c x)(d x)(e x))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)9))c d(- e 1))e)b))),,,,,,,,,9
+(define ,x(let a((b x)(c x)(d x)(e x))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)9))c d(- e 1))e)b))),,,,,,,,,,9
+(define ,x(let a((b x)(c x)(d x)(e x))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)9))c d(- e 1))e)b))),,,,,,,,,,,9
+(define ,x(let a((b x)(c x)(d x)(e x))(+(if(>(* b c d e)0)(a(a 9(- c 1)d(a(a(- b 1)c d e)b(- d 1)9))c d(- e 1))e)b))),,,,,,,,,,,,9
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 9(* n n))(x '(#f #f #f #f . #f)(p n x)))((not x)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 99(* n n))(x '(#f #f #f #f . #f)(p n x)))((not x)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 999(* n n))(x '(#f #f #f #f . #f)(p n x)))((not x)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 9(* n n))(x '(#f #f #f #f #f . #f)(p n x)))((not x)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 99(* n n))(x '(#f #f #f #f #f . #f)(p n x)))((not x)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 999(* n n))(x '(#f #f #f #f #f . #f)(p n x)))((not x)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 9(* n n))(x '(#f #f #f #f #f #f . #f)(p n x)))((not x)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 99(* n n))(x '(#f #f #f #f #f #f . #f)(p n x)))((not x)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 999(* n n))(x '(#f #f #f #f #f #f . #f)(p n x)))((not x)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 9(* n n))(x '(#f #f #f #f #f #f #f . #f)(p n x)))((not x)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 99(* n n))(x '(#f #f #f #f #f #f #f . #f)(p n x)))((not x)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 0(+ n 1))(x #f(if(> n 9)(p n x)(cons x x))))((>(if x 0 n)0)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 0(+ n 1))(x #f(if(> n 99)(p n x)(cons x x))))((>(if x 0 n)0)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 0(+ n 1))(x #f(if(> n 999)(p n x)(cons x x))))((>(if x 0 n)0)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 0(+ n 1))(x #f(if(> n 9999)(p n x)(cons x x))))((>(if x 0 n)0)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 0(+ n 1))(x #f(if(> n #e9e9)(p n x)(cons x x))))((>(if x 0 n)0)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 0(+ n 1))(x #f(if(> n #e9e99)(p n x)(cons x x))))((>(if x 0 n)0)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 0(+ n 1))(x #f(if(> n #e9e999)(p n x)(cons x x))))((>(if x 0 n)0)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 0(+ n 1))(x #f(if(> n #e9e9999)(p n x)(cons x x))))((>(if x 0 n)0)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 0(+ n 1))(x #f(if(> n #e9e99999)(p n x)(cons x x))))((>(if x 0 n)0)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 0(+ n 1))(x #f(if(> n #e9e999999)(p n x)(cons x x))))((>(if x 0 n)0)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 0(+ n 1))(x #f(if(> n #e9e9999999)(p n x)(cons x x))))((>(if x 0 n)0)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 0(+ n 1))(x #f(if(> n #e9e99999999)(p n x)(cons x x))))((>(if x 0 n)0)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 0(+ n 1))(x #f(if(> n(expt 9 #e9e9))(p n x)(cons x x))))((>(if x 0 n)0)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((n 0(+ n 1))(x #f(if(> n(expt 9 #e9e99))(p n x)(cons x x))))((>(if x 0 n)0)n))
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(define ,m(do((n 0(+ n 1))(x #f(if(> n m)(p n x)(cons x x))))((>(if x 0 n)0)n))),,9
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(define ,m(do((n 0(+ n 1))(x #f(if(> n m)(p n x)(cons x x))))((>(if x 0 n)0)n))),,,9
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(define ,m(do((n 0(+ n 1))(x #f(if(> n m)(p n x)(cons x x))))((>(if x 0 n)0)n))),,,,9
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(define ,m(do((n 0(+ n 1))(x #f(if(> n m)(p n x)(cons x x))))((>(if x 0 n)0)n))),,,,,9
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(define ,m(do((n 0(+ n 1))(x #f(if(> n m)(p n x)(cons x x))))((>(if x 0 n)0)n))),,,,,,9
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(define ,m(do((n 0(+ n 1))(x #f(if(> n m)(p n x)(cons x x))))((>(if x 0 n)0)n))),,,,,,,9
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(define ,m(do((n 0(+ n 1))(x #f(if(> n m)(p n x)(cons x x))))((>(if x 0 n)0)n))),,,,,,,,9
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(define ,m(do((n 0(+ n 1))(x #f(if(> n m)(p n x)(cons x x))))((>(if x 0 n)0)n))),,,,,,,,,9
+(define(p n x)(if(>(if(cdr x)n 0)0)(cons(p(- n 1)x)(p n(cdr x)))(car x)))(define ,m(do((n 0(+ n 1))(x #f(if(> n m)(p n x)(cons x x))))((>(if x 0 n)0)n))),,,,,,,,,,9
+(define(p n x)(if(=(if(car x)n 0)0)(cdr x)(if(eqv?(car x)0)(cons(p(- n 1)x)(cdr x))(cons(p n(car x))(p(- n 1)x)))))(do((n 9(* n n))(x '(0 0 . #f)(p n x)))((not x)n))
+(define(p n x)(if(=(if(car x)n 0)0)(cdr x)(if(eqv?(car x)0)(cons(p(- n 1)x)(cdr x))(cons(p n(car x))(p(- n 1)x)))))(do((n 9(* n n))(x '(0 0 0 0 0 0 0 0 . #f)(p n x)))((not x)n))
+(define(p x n)(if(and(> n 0)(car x)(cadr x))`(,(car x),(p(cadr x)n)(,(p(car x)n),(p x(- n 1))#f))(caddr x)))(do((n 0(+ n 1))(x #f(if(> n 9)(p x n)`(,x ,x ,x)))((>(if x 0 n)0)n)))
+
+; Research
+
+(and(cdr x)(> n 0))
+(>(if(cdr x)n 0)0)
+
+(define(p n x)(if(and(pair? x)(> n 0))(cons(p n(car x))(p(- n 1)x))(if(>(* n x)0)(cons(p n(- x 1))(p(- n 1)x)))))(do((n 9(* n n))(x 9(p n x)))((= x 0)
+
+(define(p n x)(if(and(cdr x)(> n 0))(cons(p(- n 1)x)(p n(cdr x)))(car x)))(do((k 9(-(if x 0 1)k))(m 9(if x m n))(n 0(+(if x n 0)1))(x #f(if(> n m)(p n m)(cons x x))))((= k 0)m))
+
+(define(p n x)(if(and(cdr x)(> n 0))(cons(p(- n 1)x)(p n(cdr x)))(car x)))(-(do((n 0(if x(+ n 1)(- n))(x #f(if(< n 9)(cons x x)(p(abs n)x))))((and(< n 0)x)n)))
+
+(define(p x n)(if(and(car x)(cadr x))`(,(p(car x)n)(,(car x),(p(cadr x)n),(p x(- n 1)))#f)(caddr x)))(do((n 9(* n n))(x '((#f()#f)(#f()#f)#f)(p n x)))((not x)n))
+(define(p x n)(if(and(car x)(cadr x))`(,(car x),(p(cadr x)n)(,(p(car x)n),(p x(- n 1))#f))(caddr x)))(do((n 9(* n n))(x '((#f()#f)(#f()#f)#f)(p n x)))((not x)n))
+
+(define(p n x)(if(pair? x)(if(= n 0)(car x)(cons(p(- n 1)x)(p n(cdr x))))(if(>(* n x)0)(cons(- x 1)(p(- n 1)x))0)))(let f((n 9)(x 9))(if(eq? x 0)n(f(* n n)(p n x))))
+(define(p n x)(if(pair? x)(if(> n 0)(cons(p n(car x))(p(- n 1)x))(cdr x))(if(>(* n x)0)(cons(- x 1)(p(- n 1)x))0)))(
+
+(define(p x n)(if(and(>(*(car x)n)0)(cadr x))`(,(-(car x)1),(p x(- n 1)),(car x),(p(cadr x)n). ,(cadr x))(cddr x)))(let f((n 9)(x '(9 '(0 #f . #f). #f)))(if x(f(* n n)(p x n))n))
+(define(p x n)(if(and(car x)(cadr x)(caddr x)(> n 0))`(,(p(car x)n),(p `(,(car x),(cadr x),(car x))(- n 1)),(p x(- n 1)))(caddr x)))(let f((n 9)(x '((#f #f #f)#f #f)))(if x(f(* n n)(p x n))n))
+(define (p n x)
+  (if (and (> n 0) (caar x) (cdar x))
+    (cons (cons (p n (caar x)) (cdr x))
+      (cons (cons (caar x) (p n (cdar x))) (p (- n 1) x)))
+    (cdr x)))
+(let f ((n 9)
+        (x ...))
+  (if x
+    (f (* n n) (p n x))
+    n))
